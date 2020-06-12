842a9de27814bfb862c2e9b9001f5863a349f43e
Jimmy Paulos <jpaulos@gmail.com>
2020-06-12 15:46:51 -0400

    Working BVP solves and graphical randomized tests.

--- py_opt_control/min_time_bvp.py
+++ py_opt_control/min_time_bvp.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import numpy.ctypeslib as npct
 from ctypes import c_double
@@ -21,7 +20,7 @@ def min_time_bvp(
     p1, v1, a1,
     v_min, v_max, a_min, a_max, j_min, j_max):
     """
-    Solve the minimum time state to state boundary value problem for a triple integrator.
+    Solve the minimum time state-to-state boundary value problem for a triple integrator.
 
     Inputs:
         p0, initial position, shape=(3,)
@@ -35,14 +34,19 @@ def min_time_bvp(
         j_min, j_max, jerk limits, scalars
 
     Outputs:
-        t, start time for each constant jerk segment, shape=(3,M)
-        j, jerk for each constant jerk segment, shape=(3,M)
+        t, switch times, shape=(3,M)
+        j, jerk values, shape=(3,M)
 
     The minimum time solutions are bang-coast-bang control sequences. These can
-    be described by a sequence of start times and jerk values for M
-    constant-jerk segments. The number of segments is a function of the boundary
-    values. Not every axis will have the same minimum number of segments, in
-    which case it will be prepended by time=0, jerk=0 segments.
+    be described by the initial state and a sequence of start times and jerk
+    values for several constant-jerk segments. Here the time sequence begins
+    with t=0 and ends with t=tf at the moment the final state is reached. The
+    jerk value j[k] is applied from time t[k] until t[k+1]. Note that the jerk
+    value j[-1] is irrelevant.
+
+    The optimal number of segments is a function of the boundary values. Not every axis
+    will have the same minimum number of segments, in which case it will be
+    prepended by multiple coincident time=0, jerk=0 segments.
     """
 
     # The interface uses an excess fixed sized array for the output control steps.
@@ -77,3 +81,95 @@ def min_time_bvp(
         j_final[i,-n:] = j_list[i]
 
     return t_final, j_final
+
+def switch_states(p0, v0, a0, t, j):
+    """
+    Given an initial state and an input switching sequence, compute the full
+    state at each switch time. Returned a, v, p matrices are arranged like j.
+
+    Inputs:
+        p0, initial position,     shape=(3,)
+        v0, initial velocity,     shape=(3,)
+        a0, initial acceleration, shape=(3,)
+        t, switch times, shape=(3,N)
+        j, jerk,         shape=(3,N)
+    Outputs:
+        a, acceleration, shape=(3,N)
+        v, velocity,     shape=(3,N)
+        p, position,     shape=(3,N)
+
+    For axis i at time t[i,k] the state is (p[i,k], v[i,k], a[i,k]) and a
+    constant jerk segment with value j[i,k] is initiated.
+    """
+    n_axis   = p0.shape[0]
+    n_switch = t.shape[1] # number of switch states
+
+
+    p = np.zeros((n_axis, n_switch))
+    v = np.zeros((n_axis, n_switch))
+    a = np.zeros((n_axis, n_switch))
+
+    p[:,0] = p0
+    v[:,0] = v0
+    a[:,0] = a0
+
+    for n in range(n_axis):
+        for i in range(0,  n_switch-1):
+            dt = t[n,i+1] - t[n,i]
+            p[n,i+1] = 1/6*j[n,i]*dt**3 + 1/2*a[n,i]*dt**2 + v[n,i]*dt + p[n,i]
+            v[n,i+1] = 1/2*j[n,i]*dt**2 +     a[n,i]*dt    + v[n,i]
+            a[n,i+1] =     j[n,i]*dt    +     a[n,i]
+
+    return a, v, p
+
+def sample_min_time_bvp(p0, v0, a0, t, j, dt):
+    """
+    Given an initial state and an input switching sequence, compute the full
+    state at sampled times with resolution dt.
+
+    Inputs:
+        p0, initial position,     shape=(3,)
+        v0, initial velocity,     shape=(3,)
+        a0, initial acceleration, shape=(3,)
+        t,  switch times, shape=(3,N)
+        j,  jerk,         shape=(3,N)
+        dt, time steps
+    Outputs:
+        st, times,        shape=(3,M)
+        sj, jerk,         shape=(3,M)
+        sa, acceleration, shape=(3,M)
+        sv, velocity,     shape=(3,M)
+        sp, position,     shape=(3,M)
+    """
+
+    n_axis = p0.size
+    n_switch = t.shape[1]
+
+    # It is expected that the final t for each axis are identical.
+    end_t = t[:,-1].max()
+    t[:,-1] = end_t
+
+    # Allocate dense samples over time, jerk, acceleration, velocity, position.
+    st = np.arange(t[0,0], end_t, dt)
+    if st[-1] != end_t:
+        st = np.append(st, end_t)
+    n_sample = st.size
+    sj = np.full((n_axis, n_sample), np.nan)
+    sa = np.full((n_axis, n_sample), np.nan)
+    sv = np.full((n_axis, n_sample), np.nan)
+    sp = np.full((n_axis, n_sample), np.nan)
+
+    # Accurate states at exact switching times.
+    a, v, p = switch_states(p0, v0, a0, t, j)
+
+    # Samples
+    for n in range(n_axis):
+        for i in range(n_switch-1):
+            mask = np.logical_and(t[n,i] <= st, st <= t[n,i+1])
+            dt = st[mask] - t[n,i]
+            sp[n,mask] = 1/6*j[n,i]*dt**3 + 1/2*a[n,i]*dt**2 + v[n,i]*dt + p[n,i]
+            sv[n,mask] = 1/2*j[n,i]*dt**2 +     a[n,i]*dt    + v[n,i]
+            sa[n,mask] =     j[n,i]*dt    +     a[n,i]
+            sj[n,mask] =     j[n,i]
+
+    return st, sj, sa, sv, sp
--- py_opt_control/test_min_time_bvp.py
+++ py_opt_control/test_min_time_bvp.py
@@ -2,31 +2,88 @@ import numpy as np
 import matplotlib.pyplot as plt
 import min_time_bvp
 
-# p0 = np.array([0, 1, 0], dtype=np.float64)
-# v0 = np.array([0, 1, -2], dtype=np.float64)
-# a0 = np.array([0, 0, 0], dtype=np.float64)
-# p1 = np.array([1, 3, 1], dtype=np.float64)
-# v1 = np.array([0, 1, 1], dtype=np.float64)
-# a1 = np.array([0, 0, 0], dtype=np.float64)
-
-p0 = np.array([0, 0, 0], dtype=np.float64)
-v0 = np.array([0, 0, 0], dtype=np.float64)
-a0 = np.array([0, 0, 0], dtype=np.float64)
-p1 = np.array([1, 2, -1], dtype=np.float64)
-v1 = np.array([0, 0, 0], dtype=np.float64)
-a1 = np.array([0, 0, 0], dtype=np.float64)
-
+# Constants
 v_min = -10
 v_max = 10
 a_min = -5
 a_max = 5
-j_min = -1
-j_max = 1
+j_min = -100
+j_max = 100
+
+
+
+
+# Time series plot of full state.
+p0 = np.array([0, 0.5, 0], dtype=np.float64)
+v0 = np.array([0, 0, 0], dtype=np.float64)
+a0 = np.array([0, 0, 0], dtype=np.float64)
+
+p1 = np.array([1, 2, -1], dtype=np.float64)
+v1 = np.array([0, 0, 0], dtype=np.float64)
+a1 = np.array([0, 0, 0], dtype=np.float64)
 
 (t, j) = min_time_bvp.min_time_bvp(
     p0, v0, a0,
     p1, v1, a1,
     v_min, v_max, a_min, a_max, j_min, j_max)
+a, v, p = min_time_bvp.switch_states(p0, v0, a0, t, j)
+st, sj, sa, sv, sp = min_time_bvp.sample_min_time_bvp(p0, v0, a0, t, j, dt=0.001)
 
-print(t)
-print(j)
+fig, axes = plt.subplots(4, 1, sharex=True)
+for i in range(sp.shape[0]):
+    for ax, s, l in zip(axes, [sp, sv, sa, sj], ('pos', 'vel', 'acc', 'jerk')):
+        ax.plot(st, s[i,:])
+        ax.set_ylabel(l)
+axes[3].set_xlabel('time')
+fig.suptitle('Full State over Time')
+
+
+
+
+
+# Many motions to stop at (1,1,1)
+N = 100
+p0 = np.random.uniform(-1, 1, (N,3))
+v0_master = np.random.uniform(-1, 1, (N,3))
+a0_master = np.random.uniform(-1, 1, (N,3))
+
+p1 = np.array([1, 1, 1], dtype=np.float64)
+v1 = np.array([0, 0, 0], dtype=np.float64)
+a1 = np.array([0, 0, 0], dtype=np.float64)
+
+fig, axes = plt.subplots(2, 2)
+axes = axes.flatten()
+for jj in range(3):
+    if jj == 0:
+        v0 = 0 * v0_master
+        a0 = 0 * a0_master
+        axes[jj].set_title('Zero Initial Velocity and Acceleration')
+    elif jj == 1:
+        v0 = 1 * v0_master
+        a0 = 0 * a0_master
+        axes[jj].set_title('Fixed Initial Velocity, Zero Acceleration')
+    elif jj == 2:
+        v0 = 1 * v0_master
+        a0 = 1 * a0_master
+        axes[jj].set_title('Fixed Initial Velocity and Acceleration')
+    for i in range(N):
+        (t, j) = min_time_bvp.min_time_bvp(
+            p0[i], v0[i], a0[i],
+            p1, v1, a1,
+            v_min, v_max, a_min, a_max, j_min, j_max)
+
+        a, v, p = min_time_bvp.switch_states(p0[i], v0[i], a0[i], t, j)
+        st, sj, sa, sv, sp = min_time_bvp.sample_min_time_bvp(p0[i], v0[i], a0[i], t, j, dt=0.01)
+
+        axes[jj].plot(sp[0,:], sp[1,:])
+    axes[jj].set_xlim((-0.2, 1.2))
+    axes[jj].set_ylim((-0.2, 1.2))
+    axes[jj].axis('equal')
+fig.suptitle('Motions to stop at (1,1,1)')
+
+
+
+
+
+# Show plots.
+plt.show()
